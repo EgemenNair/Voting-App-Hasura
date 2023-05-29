5f0b1df53ff044ff001bb91c49adcc8c8e7a1f25
Egemen Nair <nairegemen@gmail.com>
2023-05-30 01:08:29 +0300

    React-router-dom initialized.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,21 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-
+import Root from "./routes/root";
 import { ApolloProvider } from "@apollo/client";
 import client from "./apollo.ts";
+import ErrorPage from "./routes/error-page.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ApolloProvider client={client}>
-    <App />
+    <RouterProvider router={router} />
   </ApolloProvider>
 );
