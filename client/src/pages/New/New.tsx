2b6da3567b6980cfe35fac8646aa39f9148b5b22
Egemen Nair <nairegemen@gmail.com>
2023-05-30 15:32:00 +0300

    Created: Option, Title states.

--- client/src/pages/New/New.tsx
+++ client/src/pages/New/New.tsx
@@ -1,7 +1,50 @@
+import { useState } from "react";
+
+interface IOption {
+  title: string;
+}
+
+const initialOptions: IOption[] = [{ title: "" }];
+
 function New() {
+  const [title, setTitle] = useState("");
+  const [options, setOptions] = useState(initialOptions);
+  const handleChangeOption: ({
+    target,
+  }: {
+    target: HTMLInputElement;
+  }) => void = ({ target }) => {
+    const newArray = options;
+    newArray[Number(target.id)].title = target.value;
+    setOptions([...newArray]);
+  };
+
   return (
     <div>
       <h1>New Question</h1>
+      <h2>Question</h2>
+      <input
+        placeholder="Type your question..."
+        value={title}
+        onChange={({ target }) => setTitle(target.value)}
+      />
+      <h2>Options</h2>
+      {options.map((option, index) => {
+        return (
+          <div key={index}>
+            <input
+              placeholder="Type your option..."
+              value={option.title}
+              id={String(index)}
+              onChange={handleChangeOption}
+            />
+          </div>
+        );
+      })}
+      <button onClick={() => setOptions([...options, { title: "" }])}>
+        New Option
+      </button>
+      <button>Save</button>
     </div>
   );
 }
